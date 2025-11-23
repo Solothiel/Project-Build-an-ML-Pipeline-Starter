bb3ed4ba3432d3b455548d4bbf5233cc28042960
Solothiel <dste486@wgu.edu>
2025-11-23 16:04:33 -0700

    fixed test_data.py def test_price_range

--- main.py
+++ main.py
@@ -62,15 +62,15 @@ def go(config: DictConfig):
             },
         )
 
-        if "data_check" in active_steps:
-            mlflow.run( os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"), 
+        if "data_check" in active_steps: 
+            mlflow.run(os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
             entry_point="main",
             parameters={
-                "csv": "clean_sample.csv:latest",
-                "ref": "clean_sample.csv:reference",
-                "kl_threshold": config["data_check"]["kl_threshold"],
-                "min_price": config["etl"]["min_price"],
-                "max_price": config["etl"]["max_price"],
+                "csv": "clean_sample.csv:latest",  
+                "ref": "clean_sample.csv:reference",  
+                "kl_threshold": config["data_check"]["kl_threshold"],  
+                "min_price": config["etl"]["min_price"],  
+                "max_price": config["etl"]["max_price"],  
             },
         )
             
