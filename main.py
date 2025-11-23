d5bfff3f21d95d84b3861b049accadbfe6de4811
Solothiel <dste486@wgu.edu>
2025-11-23 15:46:43 -0700

    Updated main with data_check before updating wandb

--- main.py
+++ main.py
@@ -71,6 +71,9 @@ def go(config: DictConfig):
                 "kl_threshold": config["data_check"]["kl_threshold"],
                 "min_price": config["etl"]["min_price"],
                 "max_price": config["etl"]["max_price"],
+            },
+        )
+            
 
         if "data_split" in active_steps:
             ##################
